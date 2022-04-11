bc9ecd5a0c223449350dacaa3df663869eb1eb26
Si Ma <laura.ma0916@gmail.com>
2022-04-11 17:11:35 -0500

    update backend
    update task table and add support for update/complete task

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -9,22 +10,32 @@ from flaskr.db import get_db
 bp = Blueprint('blog', __name__)
 
 @bp.route('/')
+@login_required
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        'SELECT id, title, created, created_by, comment, is_completed'
+        ' FROM task ORDER BY id'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
+@bp.route('/<string:username>/tasklist')
+@login_required
+def get_list(username):
+    db = get_db()
+    posts = db.execute(
+        'SELECT id, title, created, created_by, comment, is_completed'
+        ' FROM task WHERE created_by = ?'
+        ' ORDER BY id', (username)
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
     if request.method == 'POST':
         title = request.form['title']
-        body = request.form['body']
+        comment = request.form['comment']
         error = None
 
         if not title:
@@ -35,9 +46,9 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
+                'INSERT INTO task (title, created_by, comment)'
                 ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                (title, g.user['username'], comment)
             )
             db.commit()
             return redirect(url_for('blog.index'))
@@ -47,16 +58,16 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
+        'SELECT t.id, title, created, created_by, comment'
+        ' FROM task t JOIN user u ON t.created_by = u.username'
+        ' WHERE t.id = ?',
         (id,)
     ).fetchone()
 
     if post is None:
-        abort(404, f"Post id {id} doesn't exist.")
+        abort(404, f"Task id {id} doesn't exist.")
 
-    if check_author and post['author_id'] != g.user['id']:
+    if check_author and post['created_by'] != g.user['username']:
         abort(403)
 
     return post
@@ -68,7 +79,7 @@ def update(id):
 
     if request.method == 'POST':
         title = request.form['title']
-        body = request.form['body']
+        comment = request.form['comment']
         error = None
 
         if not title:
@@ -79,20 +90,46 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE task SET title = ?, comment = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+                (title, comment, id)
             )
             db.commit()
             return redirect(url_for('blog.index'))
 
     return render_template('blog/update.html', post=post)
 
+@bp.route('/<int:id>/completed', methods=('GET', 'POST'))
+@login_required
+def completed(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        completed = request.form['is_done']
+        error = None
+
+        if completed == False:
+            error = 'Task could not complete yet.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE task SET is_done = ?, completed_date = ?'
+                ' WHERE id = ?',
+                (1, datetime.utcnow(), id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/completed.html', post=post)
+
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
     get_post(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.execute('DELETE FROM task WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
